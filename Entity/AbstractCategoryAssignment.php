201b724fb302c9f048e4fe7a1fe23081e2e0520e
Craig Heydenburg <craigh@mac.com>
2015-12-21 13:55:55 -0500

    Implement Symfony Form CategoryType selector.
    | Q                 | A
    | ----------------- | ---
    | Bug fix?          | no
    | New feature?      | yes
    | BC breaks?        | no
    | Deprecations?     | yes
    | Tests pass?       | yes
    | Fixed tickets     | #2455
    | Refs tickets      | -
    | License           | MIT
    | Doc PR            | included
    | Changelog updated | yes
    
     - Add (move) `Zikula\CategoriesModule\Entity\AbstractCategoryAssignment` and related documentation.
     - Replaces `Zikula\Core\Doctrine\Entity\AbstractEntityCategory` (aliased for BC).
     - Add `Zikula\CategoriesModule\Form\Type\CategoriesType` for easier category usage in Symfony Forms.

---
+++ Entity/AbstractCategoryAssignment.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright Zikula Foundation 2009 - Zikula Application Framework
+ *
+ * This work is contributed to the Zikula Foundation under one or more
+ * Contributor Agreements and licensed to You under the following license:
+ *
+ * @license GNU/LGPLv3 (or at your option, any later version).
+ * @package Zikula_Form
+ *
+ * Please see the NOTICE file distributed with this source code for further
+ * information regarding copyright and licensing.
+ */
+
+namespace Zikula\CategoriesModule\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zikula\Core\Doctrine\EntityAccess;
+use Zikula\CategoriesModule\Entity\CategoryEntity;
+
+/**
+ * Base class of many-to-many association between any entity and Category.
+ *
+ * @ORM\MappedSuperclass
+ */
+abstract class AbstractCategoryAssignment extends EntityAccess
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", name="registryId")
+     * @var integer
+     */
+    private $categoryRegistryId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Zikula\CategoriesModule\Entity\CategoryEntity")
+     * @ORM\JoinColumn(name="categoryId", referencedColumnName="id")
+     * @var CategoryEntity
+     */
+    private $category;
+
+    abstract public function getEntity();
+
+    abstract public function setEntity($entity);
+
+    public function __construct($registryId, CategoryEntity $category, $entity)
+    {
+        $this->categoryRegistryId = $registryId;
+        $this->category = $category;
+        $this->setEntity($entity);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getCategoryRegistryId()
+    {
+        return $this->categoryRegistryId;
+    }
+
+    public function setCategoryRegistryId($categoryRegistryId)
+    {
+        $this->categoryRegistryId = $categoryRegistryId;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory(CategoryEntity $category)
+    {
+        $this->category = $category;
+    }
+}
+
